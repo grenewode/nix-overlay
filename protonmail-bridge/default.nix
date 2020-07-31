581753ae2a1d44a7bf6c5a6bba07ef26b0142611
Robin M <root@grenewode.io>
2020-07-31 15:56:22 -0400

    Trying with corrected repo hash

--- protonmail-bridge/default.nix
+++ protonmail-bridge/default.nix
@@ -1,7 +1,9 @@
-{ buildGoModule, fetchFromGitHub }:
-buildGoModule rec {
+{ stdenv, go, fetchFromGitHub }:
+stdenv.mkDerivation rec {
   pname = "protonmail-bridge";
   version = "1.2.7-live.1";
+
+  nativeBuildInputs = [ go ];
 
   src = fetchFromGitHub {
     owner = "ProtonMail";
@@ -11,5 +13,11 @@ buildGoModule rec {
     sha256 = "0m3d2f16zrgs8bsa03azy36511vws6jvw2yy33vhypwikskrgx0x";
   };
 
-  subPackages = [ "cmd/Desktop-Bridge" ];
+  buildPhase = ''
+    export GOCACHE=`mktemp -d`
+    mkdir -p $GOCACHE
+
+    go build --tags nogu --tags nogui -o Desktop-Bridge cmd/Desktop-Bridge/main.go
+  '';
+
 }
