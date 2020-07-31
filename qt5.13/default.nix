af43832efaefaec0752b0767eb391a1d19ad9989
Robin M <root@grenewode.io>
2020-07-31 14:50:49 -0400

    Try to build the protonmail bridge with buildGoModule

--- qt5.13/default.nix
+++ qt5.13/default.nix
@@ -1,7 +1,12 @@
-{ git, perl, python, stdenv, bison, gperf, ruby, icu }:
-stdenv.mkDerivation {
+{ stdenv, gnumake }:
+stdenv.mkDerivation rec {
+  pname = "qt5.13";
+  version = "5.13.2";
+
+  buildInputs = [ gnumake ];
+
   src = builtins.fetchTarball {
-    url = "https://download.qt.io/archive/qt/5.13/5.13.2/single/qt-everywhere-src-5.13.2.tar.xz ";
-    sha256 = "026y0pfc17wfzav01521pd43h4r259arx9v463vg8hdy6qsjgs2m";
+    url = "https://download.qt.io/archive/qt/5.13/${version}/single/qt-everywhere-src-${version}.tar.xz";
+    sha256 = "1kmy66svc2y1zkdnp2xg90zxxyw3pqx7bdyhgcpw6jpa5bsliyf2";
   };
 }
