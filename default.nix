dc200da633272d423ed28851041a366de2564e0f
Robin M <root@grenewode.io>
2020-07-31 23:59:46 -0400

    Cleanup the old stuff

--- default.nix
+++ default.nix
@@ -1,15 +1,4 @@
-self: super: let
-  unstable = import (
-    builtins.fetchGit {
-      url = "http://jupiter.lan/gitea/grenewode/nixpkgs.git";
-      rev = "938e7c71ffd0e959813003d8235db1495d6e12c1";
-      ref = "qt5.13";
-    }
-  ) {};
-  libsForQt5 = unstable.libsForQt513;
-in
+self: super:
 {
-  protonmail-bridge-bin = libsForQt5.callPackage ./protonmail-bridge-bin {};
   protonmail-bridge = self.callPackage ./protonmail-bridge {};
-  qt513 = self.callPackage ./qt5.13 {};
 }
--- protonmail-bridge/default.nix
+++ protonmail-bridge/default.nix
@@ -1,7 +1,13 @@
-{ buildGoModule, fetchFromGitHub }:
+{ buildGoModule
+, fetchFromGitHub
+, pkg-config
+, libsecret
+}:
 buildGoModule rec {
   pname = "protonmail-bridge";
   version = "1.2.7-live.1";
+
+  nativeBuildInputs = [ pkg-config libsecret ];
 
   src = fetchFromGitHub {
     owner = "ProtonMail";
@@ -10,4 +16,10 @@ buildGoModule rec {
     rev = "v${version}";
     sha256 = "0m3d2f16zrgs8bsa03azy36511vws6jvw2yy33vhypwikskrgx0x";
   };
+
+  subPackages = [ "cmd/Desktop-Bridge" ];
+
+  vendorSha256 = "0w5gf1gn7qgw2m8rxlzdlx0ls8hcsvchh22d2ksbyz276qslbyla";
+
+  buildFlagsArray = [ "-tags='nogui'" ];
 }
