52c75df25fe6ce7049707e0d2d29b9ac3b6d4c45
Robin M <root@grenewode.io>
2020-08-02 13:42:47 -0400

    Working non-interactive bridge support

--- protonmail-bridge/default.nix
+++ protonmail-bridge/default.nix
@@ -2,6 +2,7 @@
 , fetchFromGitHub
 , pkg-config
 , libsecret
+, lib
 }:
 buildGoModule rec {
   pname = "protonmail-bridge";
@@ -17,9 +18,26 @@ buildGoModule rec {
     sha256 = "0m3d2f16zrgs8bsa03azy36511vws6jvw2yy33vhypwikskrgx0x";
   };
 
-  subPackages = [ "cmd/Desktop-Bridge" ];
+  # subPackages = [ "cmd/Desktop-Bridge" ];
 
   vendorSha256 = "0w5gf1gn7qgw2m8rxlzdlx0ls8hcsvchh22d2ksbyz276qslbyla";
 
-  buildFlagsArray = [ "-tags='nogui'" ];
+  buildPhase = ''
+    mkdir -p $out
+
+    go install \
+      -tags="pmapi_prod nogui" \
+      -ldflags="-X main.Version=${version} -X main.Revision=${src.rev}" \
+      -v -p $NIX_BUILD_CORES \
+      ./cmd/Desktop-Bridge 2>&1
+  '';
+
+  # buildFlags =
+  #   ''-tags=pmapi_prod,nogui -ldflags=-Xmain.Version=1.2.7-git,-Xmain.Revision=${src.rev} ''
+  # ;
+
+  # buildFlagsArray = [
+  #   "-tags \"pmapi_prod nogui\""
+  #   "-ldflags \"-X main.Version=1.2.7-git -X main.Revision=${src.rev}\""
+  # ];
 }
